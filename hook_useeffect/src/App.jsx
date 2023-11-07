9b353c266badb9e34463278bebba0cdc606e9768
이다원 <143374855+allone9425@users.noreply.github.com>
2023-11-07 19:53:33 +0900

    useeffect - clean up

--- hook_useeffect/src/App.jsx
+++ hook_useeffect/src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   const [value, setValue] = useState("");
   useEffect(() => {
     console.log(`hello useEffect : ${value}`);
-  }, [value]);
+
+    return (
+      () => {
+        console.log("곧 사라집니다.");
+      },
+      [value]
+    );
+  });
 
   return (
     <div>
